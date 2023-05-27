fed557a9be0a807b7c0240a778439520914a2fff
Absam <smallkid1@yahoo.com>
2023-05-27 09:36:18 +0100

    Select option values to fileld modified

--- form-request.js
+++ form-request.js
@@ -8,11 +8,11 @@ function setFormPayload() {
     console.log(payload);
     let pageForms = document.querySelectorAll("form");
     pageForms.forEach((elem, index) => {
-        section = elem.className;
+        section = elem.className.trim();
         if (section) {
             let sectionData = payload[section];
             if (sectionData) {
-                if (elem.id in sectionData) {
+                if (elem.id.trim() in sectionData) {
                     formData = sectionData[elem.id]
                     fillFormValues(formData, elem.id);
                 }
@@ -24,11 +24,11 @@ function setFormPayload() {
 async function getFormPayload(){
     let pageForms = document.querySelectorAll("form");
     pageForms.forEach((elem, index) => {
-        section = elem.className;
+        section = elem.className.trim();
         if (section) {
             registerPayloadSection(section);
             //console.log(`Section for ${elem.id}`)
-            registerFormValues(section, elem.id);
+            registerFormValues(section, elem.id.trim());
         }
     });
     return payload;
@@ -55,14 +55,15 @@ function registerFormValues(section, form_id) {
     })
     selectValues.forEach((elem, index) => {
         let key = elem.name.trim();
-        let value = elem.value.trim();
+        let value = elem.options[elem.selectedIndex].value.trim();
         payload[section][form_id][key] = value;
     })
 }
 function fillFormValues(formData, form_id) {
+    let spans = document.getElementsByTagName("span");
     let inputValues = document.querySelectorAll(`#${form_id} input`);
     let selectValues = document.querySelectorAll(`#${form_id} select`);
-    payload[section][form_id] = {}
+    //payload[section][form_id] = {}
     inputValues.forEach((elem, index) => {
         let key = elem.name.trim();
         if (key in formData)
@@ -70,7 +71,20 @@ function fillFormValues(formData, form_id) {
     })
     selectValues.forEach((elem, index) => {
         let key = elem.name.trim();
-        if (key in formData)
-            elem.value = formData[key];
+        if (key in formData) {
+            for (i = 0; i < elem.options.length; i++){
+                if (elem.options[i].value.trim() == formData[key]) {
+                    elem.options[i].selected = true;
+                    for (i = 0; i < spans.length; i++) {
+                        if (spans[i].id.includes(key)) {
+                            spans[i].textContent = formData[key];
+                            break;
+                        }
+                    }
+                }
+            }
+            // console.log(elem.options.length);
+            // elem.value = formData[key];
+        }
     })
 }
